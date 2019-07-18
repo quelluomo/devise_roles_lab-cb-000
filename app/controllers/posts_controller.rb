34a5789150439678771d05b6a14a4c565b7ffa39
Matteo Muscas <thatman.j@gmail.com>
2019-07-18 08:34:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :find_post!, only: [:edit, :show, :update]
-  before_action :authorize_user!, only: [:edit, :update]
 
 #user create read update delete own
 #vip all user + update others
@@ -44,18 +41,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
-  end
-
-  def authorize_user!
-    raise "Unauthorized!" unless current_user
-
-    unless current_user == @post.user || current_user.vip? || current_user.admin?
-      redirect_to post_path(id: @post.id), :alert => "Access denied."
-    end
-  end
-
-  def find_post!
-    @post = Post.find(params[:id])
   end
 
 end
