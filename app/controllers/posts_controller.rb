2afb2dc51b2ea58def434200d9069df81e7be0d5
Matteo Muscas <thatman.j@gmail.com>
2019-07-18 07:34:25 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,38 @@ class PostsController < ApplicationController
 #vip all user + update others
 #admin all + delete posts
 
-  def edit
+  def new
+    @post = Post.new
+  end
+
+  def create
+    user_id = current_user.id
+
+    Post.create(
+      content: params[:post][:content],
+      user_id: user_id
+    )
+
+    redirect_to user_path(id: user_id)
   end
 
 
+  def destroy
+  end
+
+  def edit
+  end
+
   def update
-    @post = Post.find(params[:id])
-    return head(:forbidden) unless current_user.admin? ||
-             current_user.vip? ||
-             current_user.try(:id) == @post.owner_id
-    @post.update(post_params)
+      if !current_user.admin? && !current_user.vip?
+
+      redirect_to :back, :alert => "Access denied."
+    else
+      @post = Post.find_by(params[:id])
+      @post.update(post_params)
+      redirect_to post_path
     end
-
-
+  end
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,40 @@
 class UsersController < ApplicationController
 
   def index
+    #@current_user = current_user
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+    if current_user.id == params[:id].to_i
+      @user = User.find(params[:id])
+    else
+      flash[:error] = "Access denied."
+      redirect_to users_path
+    end
+  end
+
+  def destroy
+    if current_user.admin?
+      @user = User.find_by(params[:id])
+      @user.destroy
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
 
 end
